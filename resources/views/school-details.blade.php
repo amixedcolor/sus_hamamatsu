c968dd4fb5c6d51fbca831027354cf0fbd8626e4
Kosuke <sasaki.kosuke.19@shizuoka.ac.jp>
2023-08-23 16:21:04 +0900

    学校詳細表示画面のHTMLタグ修正

--- resources/views/school-details.blade.php
+++ resources/views/school-details.blade.php
@@ -1,80 +1,80 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('校舎詳細表示') }}
-        </h2>
+        </h1>
     </x-slot>
 
     <div class="py-10 text-center">
-        <span class="font-bold text-xl inline-block relative inline-block bg-orange-500 text-white p-2 rounded">
+        <h2 class="font-bold text-xl inline-block relative inline-block bg-orange-500 text-white p-2 rounded">
         {{ __('浜松市立第一小学校') }}
-        </span>
-    </div>
-
-    <div class="container mx-auto w-96 h-96 bg-slate-600">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center">
-            {{ __('校舎画像等') }}
         </h2>
     </div>
 
+    <div class="container mx-auto w-96 h-96 bg-slate-600">
+        <p class="font-semibold text-xl text-gray-800 leading-tight text-center">
+            {{ __('校舎画像等') }}
+        </p>
+    </div>
+
     <div class="py-10 text-left">
-        <span class="font-bold text-xl inline-block relative inline-block mx-24">
+        <h3 class="font-bold text-xl inline-block relative inline-block mx-24">
         {{ __('費用（1時間）') }}
-            <span class="absolute h-1 w-full left-0 bottom-0 bg-orange-500"></span>
-        </span>
+            <h3 class="absolute h-1 w-full left-0 bottom-0 bg-orange-500"></h3>
+        </sph3an>
         <br>
         <br>
-        <h4 class="font-bold text-base mx-24">
+        <p class="font-bold text-base mx-24">
             5000円
-        </h4>
+        </p>
     </div>
 
     <div class="py-10 text-left">
-        <span class="font-bold text-xl inline-block relative inline-block mx-24">
+        <h3 class="font-bold text-xl inline-block relative inline-block mx-24">
         {{ __('設備') }}
-            <span class="absolute h-1 w-full left-0 bottom-0 bg-orange-500"></span>
-        </span>
+            <h3 class="absolute h-1 w-full left-0 bottom-0 bg-orange-500"></h3>
+        </h3>
         <br>
         <br>
-        <h4 class="font-bold text-base mx-24">
+        <p class="font-bold text-base mx-24">
             設備設備設備設備設備
-        </h4>
+        </p>
     </div>
 
     <div class="py-10 text-left">
-        <span class="font-bold text-xl inline-block relative inline-block mx-24">
+        <h3 class="font-bold text-xl inline-block relative inline-block mx-24">
         {{ __('貸出時間') }}
-            <span class="absolute h-1 w-full left-0 bottom-0 bg-orange-500"></span>
-        </span>
+            <h3 class="absolute h-1 w-full left-0 bottom-0 bg-orange-500"></h3>
+        </h3>
         <br>
         <br>
-        <h4 class="font-bold text-base mx-24">
+        <p class="font-bold text-base mx-24">
             X〜Y
-        </h4>
+        </p>
     </div>
 
     <div class="py-10 text-left">
-        <span class="font-bold text-xl inline-block relative inline-block mx-24">
+        <h3 class="font-bold text-xl inline-block relative inline-block mx-24">
         {{ __('連絡先') }}
-            <span class="absolute h-1 w-full left-0 bottom-0 bg-orange-500"></span>
-        </span>
+            <h3 class="absolute h-1 w-full left-0 bottom-0 bg-orange-500"></h3>
+        </h3>
         <br>
         <br>
-        <h4 class="font-bold text-base mx-24">
+        <p class="font-bold text-base mx-24">
             ○○○-○○○○-○○○○
-        </h4>
+        </p>
     </div>
 
     <div class="mb-4">
-        <span for="body" class="font-bold text-xl inline-block relative inline-block mx-24">
+        <h3 for="body" class="font-bold text-xl inline-block relative inline-block mx-24">
             備考
-            <span class="absolute h-1 w-full left-0 bottom-0 bg-orange-500"></span>
-        </span>
+            <h3 class="absolute h-1 w-full left-0 bottom-0 bg-orange-500"></h3>
+        </h3>
         <br>
         <br>
-        <h4 class="font-bold text-base mx-24">
+        <p class="font-bold text-base mx-24">
             備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考備考
-        </h4>
+        </p>
     </div>
 
     <div class="max-w-7xl mx-auto mt-10 sm:px-6 lg:px-8">
