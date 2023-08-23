02a142a2b01b193957cf82e613a7a71ad951510c
Nanai Jua <me@n7i.dev>
2023-08-23 21:45:01 +0900

    Model に関連付け (relation) を追加

--- app/Models/HeldEventImage.php
+++ app/Models/HeldEventImage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class HeldEventImage extends Model
 {
@@ -11,5 +12,10 @@ class HeldEventImage extends Model
 
     public $timestamps = false;
 
+    protected $guarded = ['id', 'held_event_id'];
     protected $fillable = ['data'];
+
+    public function heldEvent(): BelongsTo {
+        return $this->belongsTo(HeldEvent::class);
+    }
 }
--- app/Models/SchoolImage.php
+++ app/Models/SchoolImage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SchoolImage extends Model
 {
@@ -11,5 +12,10 @@ class SchoolImage extends Model
 
     public $timestamps = false;
 
+    protected $guarded = ['id', 'school_id'];
     protected $fillable = ['data'];
+
+    public function school(): BelongsTo {
+        return $this->belongsTo(School::class);
+    }
 }
