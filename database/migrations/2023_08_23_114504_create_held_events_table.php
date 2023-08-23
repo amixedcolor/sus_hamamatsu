bc32f4d7b49d2687e867012002739368a11e6447
Nanai Jua <me@n7i.dev>
2023-08-23 11:52:51 +0900

    テーブル作成の migration を作成

---
+++ database/migrations/2023_08_23_114504_create_held_events_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('held_events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('school_id');
+            $table->string('title', 64);
+            $table->string('detail', 1024);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('held_events');
+    }
+};
---
+++ database/migrations/2023_08_23_114823_create_selected_facilities_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('selected_facilities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id');
+            $table->boolean('is_gym');
+            $table->boolean('is_ground');
+            $table->boolean('is_parking');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('selected_facilities');
+    }
+};
