704e984d8b6728b25a79914d019c2079ef5395d7
akira-ujiie <2118013.ua@gmail.com>
2023-08-24 11:22:47 +0900

    コメントの変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,9 +1,5 @@
 <nav x-data="{ open: false }" >
     
-    <!-- <div style="
-    width:200px; height:64px;
-    background-color:white; 
-    "> -->
     <!-- Primary Navigation Menu -->
         
         <div class="flex justify-between h-16 bg-orange-600 pl-10">
@@ -17,74 +13,9 @@
             </h2>
         </a>
             
-            
-            <!-- ロゴと掲示板のリンク
-            <div class="flex">
-                
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
-
-                
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('post.index')" :active="request()->routeIs('post.index')" class="text-decoration-none">
-                        {{ __('掲示板') }}
-                    </x-nav-link>
-                </div>
-            </div> 
-            -->
-
-            <!-- Settings Dropdown -->
-            <!--
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                @auth
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')" class="text-decoration-none">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();"
-                                                class="text-decoration-none">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-                @else
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('register')" :active="request()->routeIs('register')" class="text-decoration-none">
-                        {{ __('Register') }}
-                    </x-nav-link>
-
-                    <x-nav-link :href="route('login')" :active="request()->routeIs('login')" class="text-decoration-none">
-                        {{ __('Login') }}
-                    </x-nav-link>
-                </div>
-                @endauth
-            </div>
-            -->
-
+        
+        
+            <!-- ハンバーガーについて　-->
             <!-- Hamburger -->
             <!--<div class="-mr-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
